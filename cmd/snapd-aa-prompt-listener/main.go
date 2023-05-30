8d86acc8eb84c691245d6c08b59b9ca0b4a33fce
Robert Ancell <robert.ancell@canonical.com>
2023-05-30 15:29:35 +1200

    Fix introspection type for io.snapcraft.AppArmorPrompt.RegisterAgent

--- cmd/snapd-aa-prompt-listener/main.go
+++ cmd/snapd-aa-prompt-listener/main.go
@@ -39,7 +39,7 @@ const aaPromptIntrospectionData = `
 <node>
 	<interface name="io.snapcraft.AppArmorPrompt">
 		<method name="RegisterAgent">
-                    <arg name="path" direction="in" type="s"/>
+                    <arg name="path" direction="in" type="o"/>
 		</method>
 	</interface>` + introspect.IntrospectDataString + `</node> `
 
